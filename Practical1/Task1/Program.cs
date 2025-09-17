99236233d6e0949ea06053345982b9b8c19f1182
LykhnoMykhailo <st9164740@stud.duikt.edu.ua>
2025-09-17 16:32:32 +0300

    second_push task1 -lab1

---
+++ Practical1/Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Task1
+{
+    using System;
+    using System.Text;
+
+    public class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            Console.WriteLine("Введіть число:");
+            string input = Console.ReadLine();
+
+
+            if (int.TryParse(input, out int number))
+            {
+                string message = GetMessage(number);
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Console.WriteLine("Будь ласка, введіть коректне ціле число.");
+            }
+        }
+        public static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+        public static string GetMessage(int number)
+        {
+            if (IsEven(number))
+            {
+                return "Двері відкриваються!";
+            }
+            else
+            {
+                return "Двері зачинені...";
+            }
+        }
+
+
+    }
+
+}
--- Practical1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Practical1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88954b75f031e40d514bc129fb52df43ccb881fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+891f1045ab1feaced3358fbdbfc9082f0fc2da19")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
